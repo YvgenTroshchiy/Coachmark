86d66e1617779dbb6a93821d96438726ec03acff
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2021-03-01 13:02:04 +0200

    Add TapTargetView library

--- app/src/main/java/com/troshchiy/coachmark/MainActivity.kt
+++ app/src/main/java/com/troshchiy/coachmark/MainActivity.kt
@@ -1,12 +1,17 @@
 package com.troshchiy.coachmark
 
 import android.os.Bundle
+import android.text.SpannableString
+import android.util.Log
 import android.view.View
 import android.view.ViewGroup
 import android.widget.FrameLayout
 import android.widget.ImageView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import com.getkeepsafe.taptargetview.TapTarget
+import com.getkeepsafe.taptargetview.TapTargetView
 
 class MainActivity : AppCompatActivity() {
 
@@ -22,9 +27,42 @@ class MainActivity : AppCompatActivity() {
 
         val rootView = anchorView.rootView as ViewGroup
         val view = OverlayView(this, anchorView)
-        rootView.addView(view)
+//        rootView.addView(view)
 
         val o = 0
+
+        anchorView.setOnClickListener { showTapTargetView() }
+    }
+
+    private fun showTapTargetView() {
+        // You don't always need a sequence, and for that there's a single time tap target
+        val spannedDesc = SpannableString("Still deciding? Favorite it for price drop alerts.")
+        //        spannedDesc.setSpan(UnderlineSpan(), spannedDesc.length - "TapTargetView".length, spannedDesc.length, 0)
+
+        TapTargetView.showFor(this, TapTarget.forView(findViewById(R.id.heart), "Want it? Heart it.", spannedDesc)
+            .outerCircleColor(R.color.white)
+            .outerCircleAlpha(0.85f)
+            .cancelable(false)
+            .drawShadow(true)
+            .dimColor(R.color.bg_dim)
+            .titleTextColor(R.color.black)
+            .titleTextDimen(R.dimen.title_text_size)
+            .tintTarget(false), object : TapTargetView.Listener() {
+            override fun onTargetClick(view: TapTargetView) {
+                super.onTargetClick(view)
+                // .. which evidently starts the sequence we defined earlier
+                //                sequence.start()
+            }
+
+            override fun onOuterCircleClick(view: TapTargetView) {
+                super.onOuterCircleClick(view)
+                Toast.makeText(view.context, "You clicked the outer circle!", Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onTargetDismissed(view: TapTargetView, userInitiated: Boolean) {
+                Log.d("TapTargetViewSample", "You dismissed me :(")
+            }
+        })
     }
 
     private fun findFrameLayout(anchorView: View): ViewGroup {
