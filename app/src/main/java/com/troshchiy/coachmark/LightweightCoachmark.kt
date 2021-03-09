c92bf3011539c308c2aed0a6b3ef21cf5fa0e0cd
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2021-03-09 16:15:36 +0200

    wip

--- app/src/main/java/com/troshchiy/coachmark/LightweightCoachmark.kt
+++ app/src/main/java/com/troshchiy/coachmark/LightweightCoachmark.kt
@@ -2,19 +2,50 @@ package com.troshchiy.coachmark
 
 import android.content.Context
 import android.graphics.PointF
+import android.util.Log
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewManager
 import android.widget.Button
 import android.widget.FrameLayout
+import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.core.view.doOnLayout
 
 class LightweightCoachmark constructor(context: Context, anchorView: View) : FrameLayout(context, null, 0) {
 
+    private val tag = this::class.java.simpleName
+
+//    private var isShown: Boolean = false
+
     init {
-        addView(LightweightCoachmarkCircle(context, getAnchorCenter(anchorView)))
+        val anchorCenter = getAnchorCenter(anchorView)
+
+        addView(LightweightCoachmarkCircle(context, anchorCenter))
 
         val dialog = LayoutInflater.from(context).inflate(R.layout.lightweight_coachmark_description, this, true)
         dialog.findViewById<Button>(R.id.gotIt).setOnClickListener { close() }
+
+        val coachmarkDescription = dialog.findViewById<LinearLayoutCompat>(R.id.coachmarkDescription)
+        coachmarkDescription.doOnLayout {
+            Log.w(tag, "w: ${coachmarkDescription.width}")
+            val horizontalPadding = context.resources.getDimension(R.dimen.horizontal_padding)
+
+            coachmarkDescription.x = anchorCenter.x - coachmarkDescription.width
+            coachmarkDescription.y = anchorCenter.y
+        }
+
+
+//        dialog.x = anchorCenter.x - dialog.width
+//        dialog.translationY = 200f
+
+
+//        val layoutParams = dialog.layoutParams as LinearLayoutCompat.LayoutParams
+
+        val layoutParams = FrameLayout.LayoutParams(LayoutParams.WRAP_CONTENT, LayoutParams.MATCH_PARENT).apply {
+            gravity = Gravity.RIGHT
+        }
+//        dialog.layoutParams = layoutParams
     }
 
     private fun close() {
@@ -22,12 +53,12 @@ class LightweightCoachmark constructor(context: Context, anchorView: View) : Fra
     }
 
     private fun getAnchorCenter(anchorView: View): PointF {
-        val anchorViewLocation = IntArray(2)
-        anchorView.getLocationInWindow(anchorViewLocation)
+        val location = IntArray(2)
+        anchorView.getLocationInWindow(location)
 
         return PointF(
-            anchorViewLocation[0].toFloat() + anchorView.width / 2,
-            anchorViewLocation[1].toFloat() + anchorView.height / 2
+            location[0].toFloat() + anchorView.width / 2,
+            location[1].toFloat() + anchorView.height / 2
         )
     }
 }
