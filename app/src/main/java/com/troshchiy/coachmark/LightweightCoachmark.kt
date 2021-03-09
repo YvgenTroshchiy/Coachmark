c026f8f861e7e93bfbe23d964ebb01ceaa748b4d
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2021-03-09 13:55:26 +0200

    Extract getAnchorCenter

--- app/src/main/java/com/troshchiy/coachmark/LightweightCoachmark.kt
+++ app/src/main/java/com/troshchiy/coachmark/LightweightCoachmark.kt
@@ -1,13 +1,13 @@
 package com.troshchiy.coachmark
 
 import android.content.Context
+import android.graphics.PointF
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.ViewManager
 import android.widget.Button
 import android.widget.FrameLayout
-import androidx.core.view.doOnLayout
 
 class LightweightCoachmark constructor(
     context: Context,
@@ -15,18 +15,24 @@ class LightweightCoachmark constructor(
     anchorView: View
 ) : FrameLayout(context, null, 0) {
 
+
     init {
-        val circle = LightweightCoachmarkCircle(context, parent, anchorView)
-        circle.doOnLayout {
-
-        }
-
-        addView(circle)
+        addView(LightweightCoachmarkCircle(context, getAnchorCenter(anchorView)))
 
         val dialog = LayoutInflater.from(context).inflate(R.layout.lightweight_coachmark_description, this, true)
         dialog.findViewById<Button>(R.id.gotIt).setOnClickListener {
             (parent as ViewManager).removeView(this@LightweightCoachmark)
         }
 
+    }
+
+    private fun getAnchorCenter(anchorView: View): PointF {
+        val anchorViewLocation = IntArray(2)
+        anchorView.getLocationInWindow(anchorViewLocation)
+
+        return PointF(
+            anchorViewLocation[0].toFloat() + anchorView.width / 2,
+            anchorViewLocation[1].toFloat() + anchorView.height / 2
+        )
     }
 }
--- app/src/main/java/com/troshchiy/coachmark/LightweightCoachmarkCircle.kt
+++ app/src/main/java/com/troshchiy/coachmark/LightweightCoachmarkCircle.kt
@@ -3,38 +3,20 @@ package com.troshchiy.coachmark
 import android.content.Context
 import android.graphics.*
 import android.view.View
-import android.view.ViewManager
-import androidx.core.view.doOnLayout
 
 class LightweightCoachmarkCircle(
     context: Context,
-    parent: ViewManager,
-    private val anchorView: View
+    var anchorCenter: PointF
 ) : View(context) {
 
     private val tag = this::class.java.simpleName
 
     private val anchorRadius = 28.dpToPx.toFloat()
-    private val anchorViewLocation = IntArray(2)
 
     private val anchorCirclePath = Path()
     private val anchorCirclePaint = Paint().apply {
         color = context.getColor(R.color.spot_highlight_green_light)
         xfermode = PorterDuffXfermode(PorterDuff.Mode.MULTIPLY)
-    }
-
-    lateinit var anchorCenter: PointF
-
-    init {
-        anchorView.doOnLayout {
-
-            anchorView.getLocationInWindow(anchorViewLocation)
-
-            anchorCenter = PointF(
-                anchorViewLocation[0].toFloat() + anchorView.width / 2,
-                anchorViewLocation[1].toFloat() + anchorView.height / 2
-            )
-        }
     }
 
     override fun onDraw(canvas: Canvas) {
