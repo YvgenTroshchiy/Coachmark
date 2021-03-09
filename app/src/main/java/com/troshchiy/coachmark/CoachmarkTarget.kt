27b985f8cbcab53441b2a1763704acccaa3ab4e6
Yvgen Troshchiy <yvgen.troshchiy@gmail.com>
2021-03-09 18:19:35 +0200

    wip

--- app/src/main/java/com/troshchiy/coachmark/CoachmarkTarget.kt
+++ app/src/main/java/com/troshchiy/coachmark/CoachmarkTarget.kt
@@ -2,23 +2,18 @@ package com.troshchiy.coachmark
 
 import android.graphics.PointF
 import android.view.View
-import androidx.core.view.doOnLayout
 
 class CoachmarkTarget(view: View) {
 
-    private val anchorViewLocation = IntArray(2)
-
-    lateinit var center: PointF
+    var center: PointF
 
     init {
-        view.doOnLayout {
+        val location = IntArray(2)
+        view.getLocationInWindow(location)
 
-            view.getLocationInWindow(anchorViewLocation)
-
-            center = PointF(
-                anchorViewLocation[0].toFloat() + view.width / 2,
-                anchorViewLocation[1].toFloat() + view.height / 2
-            )
-        }
+        center = PointF(
+            location[0].toFloat() + view.width / 2,
+            location[1].toFloat() + view.height / 2
+        )
     }
 }
